0783d360342202a5bcd307fe3e9350441f356353
Anshul24gupta <85728850+Anshul24gupta@users.noreply.github.com>
2021-06-27 13:20:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function preload(){
+seaImg = loadimage("sea.png");
+shipImg1 = loadanimation("sea.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
